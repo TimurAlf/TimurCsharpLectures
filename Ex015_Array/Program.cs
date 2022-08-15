8bf37148da1233e2df7da24729c5bf6be7c310fe
Timur <47ta@bk.ru>
2022-08-15 19:16:19 +0500

    new commit

--- Ex015_Array/Program.cs
+++ Ex015_Array/Program.cs
@@ -11,7 +11,7 @@ void PrintArray(int[] array)
     }
     Console.WriteLine();
 }
-
+/*
 void SelectionSort(int[] array)
 {
     for (int i = 0; i < array.Length - 1; i++)
@@ -20,6 +20,22 @@ void SelectionSort(int[] array)
         for (int j = i + 1; j < array.Length; j++)
         {
             if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+      Console.WriteLine();
+}
+*/
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[minPosition]) minPosition = j;
         }
         int temporary = array[i];
         array[i] = array[minPosition];
