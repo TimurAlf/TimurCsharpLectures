fa05ff6432e75166507377697e960ec0c3726bbf
Timur <47ta@bk.ru>
2022-08-15 16:24:51 +0500

    Initial commit 2

---
+++ Ex013_Cycle/Program.cs
@@ -0,0 +1,27 @@
+﻿
+/*
+string Method4(int count, string text)
+{
+    int i = 0;
+    string result = String.Empty; // пустая строка
+    while (i<count)
+    {
+        result=result + text;
+        i++;
+    }
+    return result;
+}
+string res = Method4(10, "Text");
+Console.WriteLine(res);
+*/
+string Method4(int count, string text)
+{
+    string result = String.Empty; // пустая строка
+    for (int i = 0;i<count; i++)
+    {
+        result=result + text;
+    }
+    return result;
+}
+string res = Method4(10, "Text");
+Console.WriteLine(res);
